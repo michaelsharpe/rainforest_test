c33f317cc83ded6271e05e9c783906dcb193d7b3
michaelsharpe <m.sharpe@project89.org>
2013-11-08 17:23:41 -0500

    Added log in functionality.  Adding flash alerts and notices.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 RainforestTest::Application.routes.draw do
+  get "log_in" => "sessions#new"
+  get "log_out" => "sessions#destroy"
+  get "sign_up" => "users#new"
   root :to => "products#index"
   resources :products
   resources :users, :only => [:new, :create]
+  resources :sessions, :only => [:new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
